ab436e4e85d781080accfcf0acbcf0dfebc2d3e2
Ashish Kuniyal <aashishkuniyal2024@gmail.com>
2026-07-10 01:14:32 +0530

    Time: 61 ms (94.35%), Space: 49.8 MB (62.9%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+
+        # component[i] = connected component of node i
+        component = [0] * n
+        comp = 0
+
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                comp += 1
+            component[i] = comp
+
+        ans = []
+
+        for u, v in queries:
+            ans.append(component[u] == component[v])
+
+        return ans
