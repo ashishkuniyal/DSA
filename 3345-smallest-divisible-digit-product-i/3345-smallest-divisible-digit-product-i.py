5304b93fbdb14ed42e4c491571930985ead3bff5
Ashish Kuniyal <aashishkuniyal2024@gmail.com>
2026-08-07 01:12:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.71%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -4,8 +4,8 @@ class Solution:
             product=1
             for digit in str(n):
                 product*=int(digit)
-            if product%t==0:
-                return n
+                if product%t==0:
+                    return n
             n+=1
             
         
