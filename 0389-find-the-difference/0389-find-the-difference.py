c57b350e3feb9447d423da9b9c19114dce54ccc2
Ashish Kuniyal <aashishkuniyal2024@gmail.com>
2026-08-24 01:49:10 +0530

    Time: 3 ms (51.8%), Space: 19.3 MB (62.59%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s=sorted(s)
-        t=sorted(t)
-        for i in range(len(s)):
-            if s[i]!=t[i]:
-                return t[i]
-        return t[-1]
+        count={}
+        for ch in s:
+            if ch in count:
+                count[ch]+=1
+            else:
+                count[ch]=1
+        for ch in t:
+            if ch not in count:
+                return ch
+            count[ch]-=1
+            if count[ch]<0:
+                return ch 
+       
         
